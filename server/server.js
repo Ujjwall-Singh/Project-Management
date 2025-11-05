7d075fc4a466f90ff5b48ef3362657b6ca798e40
Ujjwal Singh <145417248+Ujjwall-Singh@users.noreply.github.com>
2025-11-05 11:30:11 +0530

    fifth_commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
 
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+
 const app = express();
 
 app.use(express.json());
@@ -10,6 +13,8 @@ app.use(cors());
 app.use(clerkMiddleware());
 
 app.get('/', (req, res) => res.send('Server is live!'));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000;
 
