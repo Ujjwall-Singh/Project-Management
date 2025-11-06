7669732f6df5d89b82c5220da4eabfed16d9a4c1
Ujjwal Singh <145417248+Ujjwall-Singh@users.noreply.github.com>
2025-11-06 20:24:14 +0530

    sixth_commit

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 
+import workspaceRouter from './routes/workspaceRoutes.js';
+import { protect } from './middlewares/authMiddleware.js';
+
 const app = express();
 
 app.use(express.json());
@@ -15,6 +18,9 @@ app.use(clerkMiddleware());
 app.get('/', (req, res) => res.send('Server is live!'));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+// Routes
+app.use("/api/workspaces", protect, workspaceRouter);
 
 const PORT = process.env.PORT || 5000;
 
